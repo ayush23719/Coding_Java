a3835819d4724b171e4b38b887b783f87682f158
ayush23719 <91633771+ayush23719@users.noreply.github.com>
2022-05-30 18:34:14 +0530

    Exception handling

--- begin/src/App.java
+++ begin/src/App.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter your age");
+        // int age = sc.nextInt();
+        // Scanner sc2 = new Scanner(System.in);
+        // System.out.println("Enter your Name");
+        // String name = sc2.nextLine();
+        // System.out.println(name + "\n");
+        // System.out.println(age);
+        int arr[] = new int[5];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        arr[4] = 5;
+        try{
+            System.out.println(arr[6]);
+        }
+        catch(Exception e){
+            System.out.println(e.getLocalizedMessage());
+            System.out.println("Error");
+        }
+        System.out.println("Separate Line");
     }
 }
