4faaebf48184923ff1e9ca2436d45d037e2863b1
Ayush Pratap Singh <91633771+ayush23719@users.noreply.github.com>
2022-07-10 00:03:13 +0530

    dsa

---
+++ begin/DSA/palindrome_number.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+        
+        String s=String.valueOf(x);
+        StringBuilder backwards=new StringBuilder();
+        for(int i=s.length()-1;i>=0;i--){
+            backwards.append(s.charAt(i));
+        }
+        
+        if(backwards.toString().equals(s)){
+            return true;
+        }
+        return false;
+        
+        
+    }
+} {
+    
+}
