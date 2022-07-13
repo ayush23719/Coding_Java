576fdda3b81af1e363b1ce3a00b258b645f6c293
Ayush Pratap Singh <91633771+ayush23719@users.noreply.github.com>
2022-07-13 17:55:11 +0530

    DSA

---
+++ begin/DSA/longestValidParentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> st = new Stack<>();
+        st.push(-1);
+        int length = 0;
+        
+       if(s.length() == 0) {
+           return 0;
+       }
+       else{
+            for(int i = 0; i < s.length(); i++){
+                char ch = s.charAt(i);
+                if(ch == '('){
+                    st.push(i);
+                }
+                else{
+                    st.pop();
+                    if(st.isEmpty()){
+                        st.push(i);
+                    }
+                    else{
+                        length = Math.max(length, i-st.peek());
+                    }
+                }
+            }
+            return length;
+        } 
+    }
+}
