14d43d328b4de21b91cdce48f7016dbb4448cdac
Ayush Pratap Singh <91633771+ayush23719@users.noreply.github.com>
2022-07-13 18:00:17 +0530

    DSA

---
+++ begin/DSA/searchRange.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int n = nums.length;
+        int[] ans = {-1,-1};
+        for( int i =0; i < n; i++) {
+            if(nums[i] == target){
+                ans[0] = i;
+                break;
+            }
+        }
+        for( int i = n-1; i >= 0; i--) {
+            if(nums[i] == target){
+                ans[1] = i;
+                break;
+            }
+        }
+        return ans;
+        
+        
+    }
+} {
+    
+}
